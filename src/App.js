aeed96f281e776176a213a7f68bba67f6f0d5112
iddimsangi <adammsangi@gmail.com>
2021-12-14 20:17:14 +0300

    styled html card layout

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import img from "./images/image-equilibrium.jpg";
 import loc from "./images/icon-ethereum.svg";
 import clock from "./images/icon-clock.svg";
 import avatar from "./images/image-avatar.png";
+import view from "./images/icon-view.svg";
 function App() {
   return (
     <div className="App">
       <div className="App-div">
-        <img className="App-div--img" src={img} alt="img" />
+        <a href="#" className="App-div-container">
+        <img className="App-div-container--img-2" src={view} alt="img" />
+        <img className="App-div-container--img-1" src={img} alt="img" />
+        </a>
+
         <h2 className="App-div--heading">
           Equilibrium #3429
           <br />
@@ -24,10 +29,12 @@ function App() {
           </span>
         </div>
         <div className="App-div--footer">
-          <img src={avatar} alt="avatar" />
+      <div>
+      <img src={avatar} alt="avatar" />
           <h3>
             Creation of <span>Jules Wyvern</span>
           </h3>
+      </div>
         </div>
       </div>
     </div>
