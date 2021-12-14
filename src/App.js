3c9c406e56ea7eec0c422002198fa4bdc15bbd4a
iddimsangi <adammsangi@gmail.com>
2021-12-14 15:22:54 +0300

    add html markup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-
-import './App.scss';
-
+import "./App.scss";
+import img from "./images/image-equilibrium.jpg";
+import loc from "./images/icon-ethereum.svg";
+import clock from "./images/icon-clock.svg";
+import avatar from "./images/image-avatar.png";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-   <h1>hello world</h1>
-      </header>
+      <div className="App-div">
+        <img src={img} alt="img" />
+        <h2>
+          Equilibrium #3429
+          <br />
+          <span>Our Equilibrium collection promotes balance and calm.</span>
+        </h2>
+        <div>
+          <span>
+            <img src={loc} alt="loc" />
+            0.041ETH
+          </span>
+          <span>
+            <img src={clock} alt="loc" />
+            3days left
+          </span>
+        </div>
+        <div>
+          <img src={avatar} alt="avatar" />
+          <h3>
+            Creation of <span>Jules Wyvern</span>
+          </h3>
+        </div>
+      </div>
     </div>
   );
 }
